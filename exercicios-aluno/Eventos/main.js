f55bdd46a9b8d58f4ac610fc47d18f3ab4139a2e
Ricardo Lima <ricardo.a.lima11@aluno.senai.br>
2025-10-01 21:04:38 -0300

    Exercicios Hoje

--- exercicios-aluno/Eventos/main.js
+++ exercicios-aluno/Eventos/main.js
@@ -1,29 +1,44 @@
-const botao = document.getElementById('botao-principal');
-const input = document.getElementById('itens');
-const lista = document.getElementById('nomes');
-const contador = document.getElementById('contador');
+const botao = document.getElementById('btn-adicionar');
+const input = document.querySelector('#novo-item');
+const lista = document.querySelector('#nomes-pessoa');
 
-// Variáveis auxiliares
-let numeroDeItens
-
-botao.addEventListener('click', addArtista);
-botao.addEventListener('click', exibirValorDoCampo);
+const listaDeItens = document.querySelector('.itens');
 
 function addArtista() {
-	// Criar e adiciona novo elemento na lista
-	let artista = input.value;
-	input.value = ''; // Limpar campo de input
-	input.focus();
+	if ( item.value == '') {
+		alert('Preencha o campo item');
+		} else {
+			
+			// Div (Elemento Mãe)
+			let novoItem = document.createElement('div');
 
-    function exibirValorDoCampo() {
-	input.value;    
+			//Elementos Filhos
+			novoItem.innerHTML = `
+				<h3 class="item">${item.value}</h3>
+			 	<p class="pessoa"></p>
+				<button class="trarei">Eu Trago!</button>
+				
+			
+			`;
 
-	let itemDaLista = document.createElement('li');
-	itemDaLista.innerText = artista;
 
-	lista.appendChild(itemDaLista);
+			// Adiciona elemento na página como filho de listaDeItens
+			 listaDeItens.appendChild(novoItem);
 
-	// Incrementa contador
-	numeroDeItens= numeroDeItens+ 1;
-	contador.innerText = `${numeroDeItens} artistas`;
+			 // Adicionar evento ao botão de voluntariar-se
+			 const botaoTrazer = listaDeItens.querySelector('.trazer');
+			botaoTrazer.onclick
+		}
 }
+ function.trazerItem(evento) {
+	let botaoClicado = evento.target;
+	let divDoItem = botaoClicado.parentNode;
+
+	let paragrafo = divDoItem.querySelector('.pessoa');
+
+	paragrafo.innerText = pessoa.value
+
+	//console.log('EuTrago!');
+ }
+//Eventos
+botao.onclick = adicionaItem;
